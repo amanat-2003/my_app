bd5f1899ac3b3fe68db5dde12f6b4e81cf58e49a
Amanat Singh <amanatsinghnain@gmail.com>
2022-08-07 22:12:21 +0530

    day6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,8 +3,16 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +29,7 @@ class LoginPage extends StatelessWidget {
                 height: 20,
               ),
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
@@ -41,6 +49,14 @@ class LoginPage extends StatelessWidget {
                           hintText: "Enter Username",
                           labelText: "Username",
                         ),
+                        onChanged: (value) {
+                          name = value;
+                          setState(() {});
+                        },
+                        onTap: () {
+                          name = "😎";
+                          setState(() {});
+                        },
                       ),
                     ),
                     Container(
@@ -51,18 +67,64 @@ class LoginPage extends StatelessWidget {
                           hintText: "Enter Password",
                           labelText: "Password",
                         ),
+                        obscuringCharacter: "•",
                       ),
                     ),
                     SizedBox(
                       height: 30,
                     ),
-                    ElevatedButton(
-                      onPressed: () {
+
+                    InkWell(
+                      // splashColor: Colors.amber,
+                      // hoverColor: Colors.pink,
+                      onTap: () async {
+                        setState(() {
+                          // if (changeButton == false) {
+                            changeButton = true;
+                          // } else {
+                          //   changeButton = false;
+                          // }
+                        });
+                        await Future.delayed(Duration(milliseconds: 200));
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
-                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                      child: Text("Login"),
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 100),
+                        width: changeButton ? 60 : 150,
+                        height: 60,
+                        alignment: Alignment.center,
+                        // ignore: sort_child_properties_last
+                        child: changeButton
+                            ? Icon(
+                                Icons.done_rounded,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16),
+                              ),
+                        decoration: BoxDecoration(
+                          color: Colors.deepOrange,
+                          // shape: changeButton? BoxShape.circle : BoxShape.rectangle,
+                          borderRadius:
+                              BorderRadius.circular(changeButton ? 30 : 8),
+                        ),
+                      ),
                     ),
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    //   onLongPress: () {
+                    //     name = "😎";
+                    //     setState(() {});
+                    //   },
+                    //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    //   child: Text("Login"),
+                    // ),
                   ],
                 ),
               )
